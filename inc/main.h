476f20f2438921003bff56f8fac17f0006dafd40
dimon5360 <dimon5360@gmail.com>
2020-05-21 21:30:49 +0300

    Initial commit. Application v.0.0.1 from 21.05.2020

---
+++ inc/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* application version */
+#define MAJOR   0
+#define MINOR   0
+#define BUILD   1
+
+#endif /* MAIN_H */
--- main.cpp
+++ main.cpp
@@ -1,50 +1,21 @@
 /* user libs  ------------------------------------------------------ */
-#include "sserver.h"
 #include "main.h"
-
-/* useful libs  ---------------------------------------------------- */
-/* standard data types */
-#include <stdint.h>
-/* io lib */
 #include <iostream>
-/* smart pointers */
-#include <memory>
-/* multythread */
-#include <pthread.h>
 
-#define THREADS_NUMBER 10
+// #include <boost/asio.hpp>
+#include <boost/format.hpp>
 
-using namespace std;
-
-void *CreateTcpServer(void* threadid);
-void CreateThreads(void);
+/* Print application information */
+static void PrintAppInfo(void);
 
 int main() {
 
-    STcpServer server = STcpServer("192.168.1.69", 4059);
-    // CreateThreads();
+    PrintAppInfo();
 
     return 0;
 }
 
-void *CreateTcpServer(void* threadid) {
-
-    // shared_ptr<STcpServer> serv = 
-    //     make_shared<STcpServer>("192.168.1.69", 4059);
-    cout << "Welcome\n";
-}
-
-void CreateThreads(void) {
-
-    pthread_t threads[THREADS_NUMBER];
-    int id;
-
-    for( id = 0; id < THREADS_NUMBER; id++) {
-        int rc = pthread_create(&threads[id], NULL, CreateTcpServer, (void*)id);
-        if(rc != 0) {
-            cout << "unable to create thread/n";
-            exit(-1);
-        }
-    }
-    pthread_exit(NULL);
+static void PrintAppInfo(void) {
+    std::cout << "Application version " << 
+    boost::format("%u.%u.%u") % MAJOR % MINOR % BUILD << std::endl;
 }
