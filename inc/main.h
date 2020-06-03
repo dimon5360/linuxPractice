4ec0b150cad3c1b5a3e1068c6e7d920bf4991d7e
dimon5360 <dimon5360@gmail.com>
2020-06-03 19:09:52 +0300

    Build v.0.0.5 from 03.06.2020. Unit test for blocking wait on timer (synchronoous) for print data realized. Next step is asynchronous output.

--- inc/main.h
+++ inc/main.h
@@ -2,17 +2,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* C++ boost lib headers */ 
 #include <boost/asio.hpp>
 #include <boost/thread.hpp>
 #include <boost/format.hpp>
 #include <boost/chrono.hpp>
+#include <boost/format.hpp>
 
-/* application version v.0.0.4 from 02.06.2020 */
-#define MAJOR   0
-#define MINOR   0
-#define BUILD   4
+/* application version v.0.0.5 from 03.06.2020 */
+#define MAJOR           0
+#define MINOR           0
+#define BUILD           5
 
 /* deafult thread timeout */
-#define THREAD_TIMEOUT 5        // 5 ms
+#define THREAD_TIMEOUT  5        // 5 ms
 
 #endif /* MAIN_H */
